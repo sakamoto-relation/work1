0b777870e547e7ea17822266abf27c67cfc7d8a9
sakam <sakam@DESKTOP-M4M3CL1>
2022-09-23 23:40:23 +0900

    プロジェクト ファイルを追加します。

---
+++ WebRoot/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebRoot.Startup))]
+namespace WebRoot
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
